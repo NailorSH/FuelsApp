8ebad548cfeeab02370d733d95eae18e9c9752a9
NailorSH <nailshiyatov.04@mail.ru>
2023-04-30 12:58:00 +0300

    Something

--- app/src/main/java/com/nailorsh/fuels/ui/theme/Color.kt
+++ app/src/main/java/com/nailorsh/fuels/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.nailorsh.fuels.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Red = Color(0xFFFF0E0E)
-val DarkRed = Color(0xFFBD0000)
+//val Red = Color(0xFFFF0E0E)
+//val DarkRed = Color(0xFFBD0000)
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
